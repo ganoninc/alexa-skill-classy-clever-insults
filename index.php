9e073e60e61581cf5ea8eecd9757900574564f51
Romain Giovanetti <ganoninc@gmail.com>
2019-10-27 16:04:40 -0400

    init repo

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$insults = [];
+
+array_push($insults, "I do desire we may be better strangers.");
+array_push($insults, "You consistently meet my expectations.");
+array_push($insults, "Your ability to speak does not make you intelligent.");
+array_push($insults, "If you were half as funny as you thought you were, you'd be twice as funny as you are.");
+array_push($insults, "You’re not the dumbest person in the world, but you better hope he doesn’t die.");
+array_push($insults, "I hope your day is filled with people like you.");
+array_push($insults, "I hope your day is as pleasant as you.");
+array_push($insults, "It is impossible to underestimate you.");
+array_push($insults, "All I know is one of us is right, and the other one is you.");
+array_push($insults, "Are you an organ donor? I'd hate for your life to be a total waste.");
+array_push($insults, "I'd agree with you, but then we would both be wrong.");
+array_push($insults, "I've been called worse things by better people.");
+array_push($insults, "I envy everyone you have never met.");
+array_push($insults, "Life is full of disappointments, just ask your parents.");
+array_push($insults, "You remind me of someone. They weren't very memorable.");
+array_push($insults, "Your contribution led to quantum leaps in improvements.");
+array_push($insults, "I would challenge you to a dual of wits but I see you are unarmed.");
+array_push($insults, "If I wanted to commit suicide I'd jump from your ego to your IQ.");
+array_push($insults, "You are aware that people simply tolerate you?");
+array_push($insults, "I can explain it to you but I can't understand it for you.");
+array_push($insults, "As an outsider, what's your perspective on intelligence?");
+?>
